f76ed8d633c292ba7f8b1417223581349d24b995
Unknown <pedro.flores.albuquerque@tecnico.ulisboa.pt>
2019-03-07 23:47:41 +0000

    str_to_msgID;str_to_IP_PORT;str_to_streamID
    Funções de leitura de strings para decomposição em dados. Estão na functions.c

--- start_iamroot.c
+++ start_iamroot.c
@@ -6,51 +6,56 @@
 int main(int argc, char **argv)
 {
   int maxfd, counter;
-  char buffer[128] = {'\0'}, msg[128] = {'\0'}, resp[128] = {'\0'};
-  //enum {access_server,out,in} state;
+  char buffer[128] = {'\0'}, msg[128] = {'\0'};
   fd_set rfds;
   struct sockaddr_in addr;
-  int n,nread,fd_temp;
+  int n,nread,nw;
   unsigned int addrlen;
-
-  int newfd, nw;
+  int newfd;
   char *ptr;
 
-  //Struct to store user input
+  //Estrutura com a informação sobre o programa e dados passados pelo utilizador
   User *user=(User *)malloc(sizeof(User));
 
-  //Initialize structure with some default values
+  //Inicializar estrutura com os valores DEFAULT
   USER_init(user);
 
-  //Read valid arguments and write them to User struct
+  //Lê os dados passados pelo utilizador e escreve-os na respetiva estrutura
   if(read_args(argc, argv, user)==0)
   {
-    //List available streams if stream ID is not specified by user
+    //Apresenta lista de streams disponiveis caso nenhum tenha sido especificado
     strcpy(msg,"DUMP\n");
     reach_udp(user->rsaddr,user->rsport,msg);
-
     return 0;
   }
 
-  //Starts out of the tree
+  //Programa começa fora da àrvore
   user->state = out;
+
   while(1)
   {
+    //Reinicia os vetor de file descriptors
     FD_ZERO(&rfds);
-    FD_SET(STDIN_FILENO,&rfds); maxfd = 0;
+    FD_SET(STDIN_FILENO,&rfds); maxfd = 0; //Sets stdin file descriptor
 
+    //Procedimento para aceder ao stream
     if(user->state == out)
     {
+      //Pergunta ao servidor de raizes acerca de um servidor de acesso
       msg_in_protocol(msg,"WHOISROOT",user);
-      reach_udp(user->rsaddr,user->rsport,msg);
+      reach_udp(user->rsaddr,user->rsport,msg); //Sends and waits for answer
 
+      //Process response from root server
       if(handle_RSmessage(msg, user) == 0)
       {
         printf("Could not process response from root_server\n");
         return 0;
       }
-      if(user->state == waiting)
+
+      //There was an access server for the stream
+      if(user->state == waiting) //waiting state until 'welcome' is recieved
       {
+        //Process response from access server
         if(handle_ASmessage(msg, user) == 0)
         {
           printf("Could not process response from access_server\n");
@@ -58,45 +63,40 @@ int main(int argc, char **argv)
         }
       }
     }
-
+    //Set UDP server if program is the access server for the stream
     if(user->state == access_server)
     {
       FD_SET(user->fd_udp_serv,&rfds);maxfd=max(maxfd,user->fd_udp_serv);
     }
+    //Set TCP server if program is in the tree
     if((user->state == in)||(user->state == access_server))
     {
       FD_SET(user->fd_tcp_serv,&rfds);maxfd=max(maxfd,user->fd_tcp_serv);
     }
+    //Set TCP connection with program a montante
     if(user->state != out)
     {
       FD_SET(user->fd_tcp_mont,&rfds);maxfd=max(maxfd,user->fd_tcp_mont);
     }
-
-    printf("fd_tcp_mont:%d\n", user->fd_tcp_mont);
-    printf("fd_tcp_serv:%d\n", user->fd_tcp_serv);
-    printf("fd_udp_serv:%d\n", user->fd_udp_serv);
-    printf("maxfd:%d\n", maxfd);
-
-    printf("SELECT\n");
+    //Select from set file descriptors
     counter=select(maxfd+1,&rfds,(fd_set*)NULL,(fd_set*)NULL,(struct timeval *)NULL);
     if(counter<=0){printf("Counter <= 0\n");exit(1);}
 
     if(FD_ISSET(user->fd_udp_serv,&rfds) && user->state == access_server)
     {
-      printf("RECIEVE FROM\n");
       addrlen=sizeof(addr);
       nread=recvfrom(user->fd_udp_serv,buffer,128,0,(struct sockaddr*)&addr,&addrlen);
       if(nread==-1)/*error*/exit(1);
+
       write(1,buffer,nread);
       handle_ASmessage(buffer,user);
+
       nread = strlen(buffer);
       n=sendto(user->fd_udp_serv,buffer,nread,0,(struct sockaddr*)&addr,addrlen);
       if(n==-1)/*error*/exit(1);
-      printf("RECIEVE FROM OUT\n");
     }
     if(FD_ISSET(user->fd_tcp_serv,&rfds) && user->state != waiting)
     {
-      printf("Accept\n");
       if((newfd=accept(user->fd_tcp_serv,(struct sockaddr*)&addr,&addrlen))==-1)
       {
         printf("error: accept\n");
@@ -114,7 +114,6 @@ int main(int argc, char **argv)
     {
       if((n=read(user->fd_tcp_mont,buffer,128))!=0)
       {
-        printf("Montante falou\n");
         if(n==-1){printf("error: read\n"); exit(1);}
         ptr=&buffer[0];
         while(n>0)
@@ -124,7 +123,6 @@ int main(int argc, char **argv)
         }
       }else{
         //This close crashes select because it tries to set it again
-        printf("Montante saiu\n");
         close(user->fd_tcp_mont);
         user->state = out;
       }
@@ -154,6 +152,5 @@ int main(int argc, char **argv)
 
 }
 
-//./iamroot grupo44:193.136.138.142:59000 -i 194.210.157.208
-//./iamroot grupo44:193.136.138.142:59000 -i 194.210.157.208 -u 58001 -t 58001
+//Pedro's PC
 //./iamroot grupo44:193.136.138.142:58001 -i 192.168.1.67 -u 58001 -t 58001
